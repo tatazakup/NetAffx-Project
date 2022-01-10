981645d92fd296731c41b709c95c78ce8034900b
chayanuttan <72799502+chayanuttan@users.noreply.github.com>
2022-01-10 22:06:27 +0700

    Update mapSNPwithDisease.py

--- Project/Class/mapSNPwithDisease.py
+++ Project/Class/mapSNPwithDisease.py
@@ -6,13 +6,14 @@ class mapSNPwithDis:
         self.ListGeneSNP = []
         self.ListGenePathway = []
         self.ListGeneDisease = []
+        self.ListGeneFromSource = []
         self.ListGeneMatchDisease = []
 
     def query(self):
         database = Database()
         conn = database.ConnectDatabase()
 
-        sqlGeneSNP = "SELECT GENE_ID, RS_ID FROM gene_snp;"
+        sqlGeneSNP = "SELECT GENE_ID, RS_ID, GENE_SYMBOL FROM gene_snp;"
         resultGeneSNP = database.CreateTask(conn, sqlGeneSNP, ())
         for i in resultGeneSNP:
             self.ListGeneSNP.append(i)
@@ -27,11 +28,17 @@ class mapSNPwithDis:
         for i in resultGeneDisease:
             self.ListGeneDisease.append(i)
 
+        sqlGeneSource = "SELECT * FROM gene_disease_source;"
+        resultGeneSource = database.CreateTask(conn, sqlGeneSource, ())
+        for i in resultGeneSource:
+            self.ListGeneFromSource.append(i)
+
         database.CloseDatabase(conn)
     
     def Map(self):
         df_ListGeneDisease = pd.DataFrame(self.ListGeneDisease, columns= ['DISEASE_ID', 'GENE_ID'])
         df_ListGenePathway = pd.DataFrame(self.ListGenePathway, columns= ['DISEASE_ID', 'GENE_ID'])
+        df_ListGeneSource = pd.DataFrame(self.ListGeneFromSource, columns= ['GENE_SYMBOL', 'SOURCE_WEBSITE'])
         for i in self.ListGeneSNP:
             print("geneid :", i[0])
             print("----check in disease-----")
@@ -40,9 +47,15 @@ class mapSNPwithDis:
                 print("not match")
             else:
                 listDis = matchGeneDis["DISEASE_ID"].values.tolist()
+
+                print('GSYMBOL :', i[2])
+                checksource = df_ListGeneSource.loc[df_ListGeneSource['GENE_SYMBOL'] == i[2]]
+                listSource = checksource['SOURCE_WEBSITE'].values.tolist()
                 for DisID in listDis:
-                    value = [i[1], DisID]
-                    self.ListGeneMatchDisease.append(value)
+                    for Sourceweb in listSource:
+                        matchby = 'Gene In Disease From Web ' + Sourceweb
+                        value = [i[1], DisID, matchby]
+                        self.ListGeneMatchDisease.append(value)
             print("----check in pathway-----")        
             matchGenePathway =df_ListGenePathway.loc[df_ListGenePathway['GENE_ID'] == i[0]]
             if matchGenePathway.empty:
@@ -50,7 +63,7 @@ class mapSNPwithDis:
             else:
                 listDisPathway = matchGenePathway["DISEASE_ID"].values.tolist()
                 for DisID in listDisPathway:
-                    valuepathway = [i[1], DisID]
+                    valuepathway = [i[1], DisID,  'Gene in Pathway']
                     self.ListGeneMatchDisease.append(valuepathway)
             print(" ")
                 
@@ -59,7 +72,7 @@ class mapSNPwithDis:
         database = Database()
         conn = database.ConnectDatabase()
         for matching in self.ListGeneMatchDisease:
-            sql = "REPLACE INTO matching_snp_disease(RS_ID, DISEASE_ID) VALUES(%s, %s)"
+            sql = "REPLACE INTO matching_snp_disease(RS_ID, DISEASE_ID, MatchBy) VALUES(%s, %s, %s)"
             val = (matching)
             resultGeneDisease = database.CreateTask(conn, sql, val)
             print("save success")
